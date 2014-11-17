8c46514163f2ae43c4d82e85556868a4f3f4d59e
AirCake <nrgstar@Ido-Cohens-Mac-mini.local>
2014-11-17 18:26:14 +0000

    Nov-17-2014 18:25

--- Quiz/ViewController.m
+++ Quiz/ViewController.m
@@ -9,11 +9,44 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UILabel *questionLabel;
+@property (nonatomic) int currentQuestionIndex;
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
+@property (weak, nonatomic) IBOutlet UILabel *answerLabel;
+@property (weak,nonatomic) IBOutlet UILabel *questionLabel;
 @end
 
 @implementation ViewController
 
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    //Call the init method implemented by the superclass
+    self = [ super initWithNibName:<#nibNameOrNil#> bundle:<#nibBundleOrNil#>];
 
+    if (self) {
+        //Create two arrays filled with questions and answers
+        //and makee the pointers point to them
+        
+        self.questions = @[@"What is cognac made of?",
+                           @"What is 7+7?",
+                           @"What is the capital of vermont?"
+                           ];
+        self.answers = @[@"Grapes",
+                         @"14",
+                         @"Montpellier"
+                         ];
+}
+    //Return the address of the new object
+    return self;
+}
 
+- (IBAction)showQuestion:(id)sender
+{
+    
+}
+- (IBAction)showAnswer:(id)sender
+{
+    
+}
 @end
