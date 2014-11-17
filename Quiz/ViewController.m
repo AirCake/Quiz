5d3940e019193aa3a22cc8da6acfe4aece8b075a
AirCake <nrgstar@Ido-Cohens-Mac-mini.local>
2014-11-17 17:46:56 +0000

    Quiz
    A Quiz app

--- Quiz/ViewController.m
+++ Quiz/ViewController.m
@@ -2,26 +2,18 @@
 //  ViewController.m
 //  Quiz
 //
-//  Created by Ido Cohen on 17/11/2014.
+//  Created by S on 17/11/2014.
 //  Copyright (c) 2014 BNR. All rights reserved.
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *questionLabel;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
